1ae6dc38dd2e13ab8245a536c6e09453e161f29b
Jerry Cao <jerry.cao@amlogic.com>
2011-05-05 16:40:08 -0700

    Use LOCAL_REQUIRED_MODULES instead of LOCAL_JNI_SHARED_LIBRARIES for HDMI JNI

--- Android.mk
+++ Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := HdmiSwitch
 LOCAL_CERTIFICATE := platform
 
-LOCAL_JNI_SHARED_LIBRARIES := libhdmiswitchjni
+LOCAL_REQUIRED_MODULES := libhdmiswitchjni
 include $(BUILD_PACKAGE)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
