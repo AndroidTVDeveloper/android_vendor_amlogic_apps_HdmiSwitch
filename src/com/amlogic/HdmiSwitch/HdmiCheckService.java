36f46674f64d7514ca0650867a71e704d1e63494
eric <eric.zhong@amlogic.com>
2011-10-17 17:05:26 +0800

    log: v2.2, check for mode switch

--- src/com/amlogic/HdmiSwitch/HdmiCheckService.java
+++ src/com/amlogic/HdmiSwitch/HdmiCheckService.java
@@ -116,6 +116,13 @@ public class HdmiCheckService extends Service {
                  	if (!HdmiSwitch.getCurMode().equals("panel")) {
                  		sendBroadcast( new Intent(ACTION_HDMISWITCH_MODE_CHANGED));
                  		sendTvOutIntent(false);                 		
+                 		// delay for finish intent              		
+                 		try {
+                 			Thread.currentThread().sleep(1000);
+                 		} catch (InterruptedException e) {
+                 			// TODO Auto-generated catch block
+                 			e.printStackTrace();
+                 		}  
                  		HdmiSwitch.setMode("panel");
                  	}
                  	
