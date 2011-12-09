9763bf0aed739652f077bd590c6412acd874eea7
michael.zhang <michael.zhang@amlogic.com>
2011-12-09 21:41:13 +0800

    Update hdmi status when bootup

--- src/com/amlogic/HdmiSwitch/HdmiBroadcastReceiver.java
+++ src/com/amlogic/HdmiSwitch/HdmiBroadcastReceiver.java
@@ -9,6 +9,10 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.WindowManagerPolicy;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HdmiBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "HdmiBroadcastReceiver";
 
@@ -17,9 +21,51 @@ public class HdmiBroadcastReceiver extends BroadcastReceiver {
         
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {						
+	        boolean plugged = false;
+	        // watch for HDMI plug messages if the hdmi switch exists
+	        if (new File("/sys/devices/virtual/switch/hdmi/state").exists()) {	
+	            final String filename = "/sys/class/switch/hdmi/state";
+	            FileReader reader = null;
+	            try {
+	                reader = new FileReader(filename);
+	                char[] buf = new char[15];
+	                int n = reader.read(buf);
+	                if (n > 1) {
+	                    plugged = 0 != Integer.parseInt(new String(buf, 0, n-1));
+	                }
+	            } catch (IOException ex) {
+	                Log.w(TAG, "Couldn't read hdmi state from " + filename + ": " + ex);
+	            } catch (NumberFormatException ex) {
+	                Log.w(TAG, "Couldn't read hdmi state from " + filename + ": " + ex);
+	            } finally {
+	                if (reader != null) {
+	                    try {
+	                        reader.close();
+	                    } catch (IOException ex) {
+	                    }
+	                }
+	            }
+	        }
+	        
+	        if (plugged) {
+                NotificationManager nM = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+                
+                CharSequence text = context.getText(R.string.hdmi_state_str1);     
+                Notification notification = new Notification(R.drawable.stat_connected, text, System.currentTimeMillis());  
+
+                Intent it = new Intent(context, HdmiSwitch.class);
+                it.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                PendingIntent contentIntent = PendingIntent.getActivity(context, 0, it, 0);        
+                notification.setLatestEventInfo(context, context.getText(R.string.app_name), text, contentIntent);
+
+                nM.notify(HDMI_NOTIFICATIONS, notification);	        
+	        }
+	        		
+        }    	
 
         if (WindowManagerPolicy.ACTION_HDMI_HW_PLUGGED.equals(intent.getAction())) {
-            Log.d(TAG, "onReceive: " + intent.getAction());
+            //Log.d(TAG, "onReceive: " + intent.getAction());
             boolean plugged = intent.getBooleanExtra(WindowManagerPolicy.EXTRA_HDMI_PLUGGED_STATE, false); 
             if(plugged){
                 NotificationManager nM = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
