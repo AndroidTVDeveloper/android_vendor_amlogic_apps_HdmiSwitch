7243a67fa210c6e9ebf4dc0e5570286a32c36b53
Stark Li <stark.li@amlogic.com>
2014-11-25 09:55:13 +0800

    HdmiSwitch for 5.0
    Change-Id: Ic7871a50b0e6c418c32a7249cc5008fa9e5d5fc4

--- Android.mk
+++ Android.mk
@@ -5,11 +5,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_JAVA_LIBRARIES := droidlogic
+LOCAL_SDK_VERSION := current
 
 LOCAL_PACKAGE_NAME := HdmiSwitch
 LOCAL_CERTIFICATE := platform
 
-LOCAL_REQUIRED_MODULES := libhdmiswitchjni
 include $(BUILD_PACKAGE)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
